2bd5733b57b026ca2a00ffd9b9de0d004ef21cfa
WataruSugiura <38929316+flat35hdapp@users.noreply.github.com>
2020-03-18 22:50:33 +0900

    deploy firstview

--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,14 @@ const routes = [
     meta: {title: 'TODOアプリ　一覧画面', desc: 'TODOアプリの一覧画面です。TODOのリストを確認できます。'},
     component: Home
   },
-  {
+  /*{
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import('../views/About.vue')
+  }*/
 ]
 
 const router = new VueRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <section>
+      <v-parallax :src="imgLink.firstView" height="600">
+        <v-layout column align-center justify-center class="white--text">
+          <h1
+              class="white--text mb-2 display-1 text-xs-center"
+            >キャッチコピー募集！
+          </h1>
+          <div
+              class="white--text subheading mb-3 text-xs-center"
+              style="font-weight: 900; text-shadow: 2px 2px #000000; border-bottom: solid 2px red;"
+            >サブタイトルください！！</div>
+        </v-layout>
+      </v-parallax>
+    </section>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
+  },
+  data: function () {
+    return {
+      imgLink: {
+        firstView: 'https://live.staticflickr.com/65535/48247140977_d4ea623259_o.jpg',
+      }
+    }
   }
 }
 </script>
